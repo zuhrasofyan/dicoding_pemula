c025c8281f239428aad23ec41a309da24af85c93
Zuhra <zuhrasofyan@gmail.com>
2019-08-21 17:11:39 +0700

    Intent - Intent dengan ResultActivity

--- MyIntentApp/app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.java
+++ MyIntentApp/app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.java
@@ -2,16 +2,22 @@ package com.dicoding.picodiploma.myintentapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnMoveActivity;
     Button btnMoveWithDataActivity;
     Button btnMoveWithObject;
     Button btnDialPhone;
+    Button btnMoveWithResult;
+    TextView tvResult;
+
+    private int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnDialPhone = findViewById(R.id.btn_dial_number);
         btnDialPhone.setOnClickListener(this);
+
+        btnMoveWithResult = findViewById(R.id.btn_move_for_result);
+        btnMoveWithResult.setOnClickListener(this);
+
+        tvResult =findViewById(R.id.tv_result);
     }
 
     @Override
@@ -60,8 +71,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber ));
                 startActivity(dialPhoneIntent);
                 break;
+            case R.id.btn_move_for_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+                startActivityForResult(moveForResultIntent, REQUEST_CODE);
+                break;
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MoveForResultActivity.RESULT_CODE) {
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText(String.format("Hasil: %s", selectedValue));
+            }
+        }
+    }
 }
