a5345e6766f94dd7d567d97b85f9a1987d471ecc
Zuhra <zuhrasofyan@gmail.com>
2019-08-22 10:50:10 +0700

    Fragment - Flexible Fragment di Satu Activity

---
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/picodiploma/myflexiblefragment/CategoryFragment.java
@@ -0,0 +1,45 @@
+package com.dicoding.picodiploma.myflexiblefragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_detail_category) {
+
+        }
+    }
+}
