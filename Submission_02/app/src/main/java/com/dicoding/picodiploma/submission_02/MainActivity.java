1cc42c20d9008d533163b19d86bc1548259c45dc
Zuhra <zuhrasofyan@gmail.com>
2019-09-16 18:17:09 +0700

    INIT

---
+++ Submission_02/app/src/main/java/com/dicoding/picodiploma/submission_02/MainActivity.java
@@ -0,0 +1,40 @@
+package com.dicoding.picodiploma.submission_02;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_movie:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_tvshow:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
