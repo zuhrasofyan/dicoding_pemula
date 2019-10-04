ce70e1372bfa0c1793d29d3a1ba815e775116e9a
Zuhra <zuhrasofyan@gmail.com>
2019-10-04 17:27:50 +0700

    Sub_02 bottomNav only for Movie and TV Show

--- Sub_02/app/src/main/java/com/dicoding/picodiploma/sub_02/MainActivity.java
+++ Sub_02/app/src/main/java/com/dicoding/picodiploma/sub_02/MainActivity.java
@@ -16,14 +16,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_movie:
+                    mTextMessage.setText(R.string.title_movie);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_tv_show:
+                    mTextMessage.setText(R.string.title_tv_show);
                     return true;
             }
             return false;
