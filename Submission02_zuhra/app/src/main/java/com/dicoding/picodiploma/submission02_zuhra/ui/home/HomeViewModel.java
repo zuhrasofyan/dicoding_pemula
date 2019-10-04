768fc8e0dd722f3c9a37e7427192bcbec58593c8
Zuhra <zuhrasofyan@gmail.com>
2019-10-04 19:15:32 +0700

    Submission02_zuhra INIT

---
+++ Submission02_zuhra/app/src/main/java/com/dicoding/picodiploma/submission02_zuhra/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.dicoding.picodiploma.submission02_zuhra.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
