a46ee8a547f46f0b2aff7dc6cf2635fe761b435f
Zuhra <zuhrasofyan@gmail.com>
2019-10-04 18:11:00 +0700

    Add fragment movie and tv show

--- Sub_02/app/src/main/java/com/dicoding/picodiploma/sub_02/MainActivity.java
+++ Sub_02/app/src/main/java/com/dicoding/picodiploma/sub_02/MainActivity.java
@@ -2,6 +2,7 @@ package com.dicoding.picodiploma.sub_02;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -15,12 +16,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
             switch (item.getItemId()) {
                 case R.id.navigation_movie:
-                    mTextMessage.setText(R.string.title_movie);
+                    fragment = new MovieFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName()).commit();
                     return true;
                 case R.id.navigation_tv_show:
-                    mTextMessage.setText(R.string.title_tv_show);
+                    fragment = new TvShowFragment();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName()).commit();
                     return true;
             }
             return false;
@@ -32,8 +36,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (savedInstanceState==null) {
+            navView.setSelectedItemId(R.id.navigation_movie);
+        }
     }
 
 }
